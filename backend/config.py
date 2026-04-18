8780f81473d2a1601018bbcfb1ff6f1da9d194ef
czr <czr@jmu.edu.cn>
2026-04-18 14:12:53 +0800

    初始化

---
+++ backend/config.py
@@ -0,0 +1,23 @@
+# 配置文件（不是自己的，注释即可）
+# 这是林宸葳的配置，其他人请勿乱动！
+# LLM_BASE_URL = "http://192.168.16.1:1234/v1"
+# LLM_MODEL_NAME = "gemma-3-4b"
+# LLM_API_KEY = "dummy"
+# 这是葛福荣的配置，其他人请勿乱动！
+# LLM_BASE_URL = "http://192.168.243.1:1234/v1"
+# LLM_MODEL_NAME = "qwen3.5"
+# LLM_API_KEY = "dummy"
+#  这是王婧琦的配置，其他人请勿乱动！
+# LLM_BASE_URL = "http://169.254.138.246:1234/v1"
+# LLM_MODEL_NAME = "qwen3.5-0.8b"
+# LLM_API_KEY = "dummy"
+# 这是蔡子荣的配置，其他人请勿乱动！
+# LLM_BASE_URL = "http://192.168.243.1:1234/v1"
+# LLM_MODEL_NAME = "qwen3.5"
+# LLM_API_KEY = "dummy"
+# 这是张书涵的配置，其他人请勿乱动！
+LLM_BASE_URL = "http://192.168.243.1:1234/v1"
+LLM_MODEL_NAME = "qwen3.5-0.8b"
+LLM_API_KEY = "dummy"
+
+#haihaihai
